915847c8e11ea74606ebfcc995727e272f376fad
Daniela-Marinela Bistrean <daniela.marinela.bistrean@gmail.com>
2019-05-18 14:23:31 +0300

    Add UE and gNB nodes

---
+++ src/nodes/abstraction/NetworkAbstraction.cc
@@ -0,0 +1,53 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#include "NetworkAbstraction.h"
+
+NetworkAbstraction&
+NetworkAbstraction::getInstance ()
+{
+    static NetworkAbstraction s_instance;
+    return s_instance;
+}
+
+void
+NetworkAbstraction::registerUser (UE * pUser)
+{
+    if (! pUser)
+        return;
+
+    m_users [pUser->getId ()] = pUser;
+}
+
+void
+NetworkAbstraction::deregisterUser (UE * pUser)
+{
+    if (! pUser)
+        return;
+
+    m_users.erase (pUser->getId ());
+}
+
+void
+NetworkAbstraction::registerBase (gNB * base)
+{
+    m_base = base;
+}
+
+void
+NetworkAbstraction::deregisterBase ()
+{
+    m_base = nullptr;
+}
---
+++ src/nodes/abstraction/NetworkAbstraction.h
@@ -0,0 +1,47 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#ifndef NODES_ABSTRACTION_NETWORKABSTRACTION_H_
+#define NODES_ABSTRACTION_NETWORKABSTRACTION_H_
+
+#include <map>
+
+#include "../UE/UE.h"
+#include "../gNB/gNB.h"
+
+class NetworkAbstraction {
+public:
+    static NetworkAbstraction& getInstance ();
+
+public:
+    NetworkAbstraction (const NetworkAbstraction &) = delete;
+    NetworkAbstraction& operator= (const NetworkAbstraction &) = delete;
+
+public:
+    void registerUser (UE * pUser);
+    void deregisterUser (UE * pUser);
+
+    void registerBase (gNB * base);
+    void deregisterBase ();
+
+private:
+    NetworkAbstraction () : m_base {nullptr} {};
+
+private:
+    std::map <int, UE *> m_users;
+    gNB * m_base;
+};
+
+#endif /* NODES_ABSTRACTION_NETWORKABSTRACTION_H_ */
